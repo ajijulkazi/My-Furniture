a4e4bb8198c61c760261a2886437c19eea513f8a
ajijul-kazi <ajijulkazi0178@gmail.com>
2023-04-06 19:56:02 +0600

    read all product in product page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,27 @@
 import React, { useContext } from 'react';
 import { ProductContext } from '../contexts/ProductContext';
+import Product from '../components/Product';
 const Homepage = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
-  console.log(products);
+  // get only men's women's category
+  const filteredProducts =products.filter( item => {
+    return  ( item.category === "men's clothing" || item.category === "women's clothing");
+  });
   return (
-    <div className=""><h2>home</h2></div>
+    <div>
+      <section className='py-20'>
+        <div className="container mx-auto">
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+            {filteredProducts.map(product => {
+              return (
+                <Product product={product} key={ product.id }/>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
   );
   }
   
