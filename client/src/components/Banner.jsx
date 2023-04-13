faf0a9f6f5033584c27b96b4290557617a762c8a
ajijul-kazi <ajijulkazi0178@gmail.com>
2023-04-13 17:42:31 +0600

    all comment remove from bannerSlider

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -41,7 +41,6 @@ const BannerSlider = () => {
             className="bg-center bg-cover h-80 sm:h-96"
             style={{ backgroundImage: `url(${banner.image})` }}
           >
-            {/* <h1 className="text-4xl text-white font-bold py-8">{banner.title}</h1> */}
           </div>
         </div>
       ))}
@@ -50,30 +49,3 @@ const BannerSlider = () => {
 };
 
 export default BannerSlider;
-
-
-
-
-// import React from 'react';
-// import productImg from '../img/product1.jpg';
-// const Banner = () => {
-//     return (
-//         <section className=' bg-pink-200 h-full bg-hero'>
-//             <div className="container max-auto flex justify-around h-full">
-//                 <div className="flex">
-//                     <div>
-//                         <div className=' mt-48 pl-10'>New Trend</div>
-//                     </div>
-//                 </div>
-//                 <div>
-//                     <img src={productImg} alt=''/>
-//                 </div>
-//                 <div>
-//                         <div className=' mt-48'>Coming Soon...</div>
-//                     </div>
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default Banner;
